8f45b738e973c3cd0ff10a7a3f9df3de554bfedc
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-02-25 23:32:21 -0800

    Fix output

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -5,7 +5,7 @@ import importlib
 
 backend = ["mxnet", "tensorflow", "theano"]
 metrics = ["training_time", "training_memory", "training_accuracy", "test_accuracy"]
-module_name = ["mnist_hierarchical_rnn_cpu", "addition_rnn_cpu", "babi_rnn_cpu"]
+module_name = ["addition_rnn_cpu", "babi_rnn_cpu", "mnist_hierarchical_rnn_cpu"]
 result = dict()
 test_summary = open('test_summary.txt', 'w')
 
@@ -20,8 +20,7 @@ for back in backend:
         result[back][module] = copy.deepcopy(example.ret_dict)
         del sys.modules[module]
 
-output = ''
-for back in backend:
+    output = ''
     output += "{backend:<20}\n".format(backend=back)
     output += "{describe:<20}".format(describe='exampe/metric')
     for metric in metrics:
@@ -33,6 +32,5 @@ for back in backend:
             output += "{metric:<25}".format(metric=result[back][module][metric])
         output += '\n'
     output += '\n'
-
-test_summary.write(output)
-print output
+    test_summary.write(output)
+    print output
